bfbd0e544fc7f168b2335df4bf808b7f866b955e
Rishabh Saxena <saxena.ris@northeastern.edu>
2024-10-13 11:47:56 -0400

    diabetes predictor coded (bad accuracy)

---
+++ diabetes_prediction.py
@@ -0,0 +1,167 @@
+# pytorch mlp for binary classification of diabetes dataset
+from numpy import vstack
+from pandas import read_csv
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torch import Tensor
+from torch import from_numpy
+from torch.nn import Linear
+from torch.nn import ReLU
+from torch.nn import Sigmoid
+from torch.nn import Module
+from torch.optim import SGD
+from torch.nn import BCELoss
+from torch.nn.init import kaiming_uniform_
+from torch.nn.init import xavier_uniform_
+
+# dataset definition
+class CSVDataset(Dataset):
+    # load the dataset
+    def __init__(self, path):
+        try:
+            df = read_csv(path, header=0)
+        except FileNotFoundError:
+            print(f"File {path} not found.")
+            raise
+        # store the inputs and outputs
+        # IMPLEMENT SCALING LATER
+        self.X = df.iloc[:,:-1].values.astype('float32')
+        self.y = df.iloc[:,-1].values # Last column - title of column
+        # ensure inputs are floats
+        self.X = self.X.astype('float32')
+        # label encode target and ensure targets are floats
+        self.y = LabelEncoder().fit_transform(self.y)
+        self.y = self.y.astype('float32')
+        self.y = self.y.reshape((len(self.y), 1))
+
+    def __repr__(self):
+        return f"Dataset contains {len(self.X)} samples."
+    # number of rows in the dataset
+    def __len__(self):
+        return len(self.X)
+
+    # get a row at an index
+    def __getitem__(self, idx):
+        X = from_numpy(self.X[idx])
+        y = from_numpy(self.y[idx])
+        return (X, y)
+
+    # get indexes for train and test rows
+    def get_splits(self, n_test=0.33):
+        # determine sizes
+        test_size = round(n_test * len(self.X))
+        train_size = len(self.X) - test_size
+        # calculate the split
+        return random_split(self, [train_size, test_size])
+
+# model definition
+class MLP(Module):
+    # define model elements
+    def __init__(self, n_inputs):
+        super(MLP, self).__init__()
+        # input to first hidden layer
+        self.hidden1 = Linear(n_inputs, 10)
+        kaiming_uniform_(self.hidden1.weight, nonlinearity='relu')
+        self.act1 = ReLU()
+        # second hidden layer
+        self.hidden2 = Linear(10, 8)
+        kaiming_uniform_(self.hidden2.weight, nonlinearity='relu')
+        self.act2 = ReLU()
+        # third hidden layer and output
+        self.hidden3 = Linear(8, 1)
+        xavier_uniform_(self.hidden3.weight)
+        self.act3 = Sigmoid()
+ 
+    # forward propagate input
+    def forward(self, X):
+        # input to first hidden layer
+        X = self.hidden1(X)
+        X = self.act1(X)
+         # second hidden layer
+        X = self.hidden2(X)
+        X = self.act2(X)
+        # third hidden layer and output
+        X = self.hidden3(X)
+        X = self.act3(X)
+        return X
+    
+# prepare the dataset
+def prepare_data(path):
+    # load the dataset
+    dataset = CSVDataset(path)
+    # calculate split
+    train, test = dataset.get_splits()
+    # prepare data loaders
+    train_dl = DataLoader(train, batch_size=32, shuffle=True)
+    test_dl = DataLoader(test, batch_size=1024, shuffle=False)
+    return train_dl, test_dl
+
+# train the model
+def train_model(train_dl, model):
+    # define the optimization
+    criterion = BCELoss()
+    optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9)
+    # enumerate epochs
+    for epoch in range(100):
+        # enumerate mini batches
+        for i, (inputs, targets) in enumerate(train_dl):
+            # clear the gradients
+            optimizer.zero_grad()
+            # compute the model output
+            yhat = model(inputs)
+            # calculate loss
+            loss = criterion(yhat, targets)
+            # credit assignment
+            loss.backward()
+            # update model weights
+            optimizer.step()
+
+# evaluate the model
+def evaluate_model(test_dl, model):
+    predictions, actuals = list(), list()
+    for i, (inputs, targets) in enumerate(test_dl):
+        # evaluate the model on the test set
+        yhat = model(inputs)
+        # retrieve numpy array
+        yhat = yhat.detach().numpy()
+        actual = targets.numpy()
+        actual = actual.reshape((len(actual), 1))
+        # round to class values
+        yhat = yhat.round()
+        # store
+        predictions.append(yhat)
+        actuals.append(actual)
+    predictions, actuals = vstack(predictions), vstack(actuals)
+    # calculate accuracy
+    acc = accuracy_score(actuals, predictions)
+    return acc
+
+# make a class prediction for one row of data
+def predict(row, model):
+    # convert row to data
+    row = Tensor([row])
+    # make prediction
+    yhat = model(row)
+    # retrieve numpy array
+    yhat = yhat.detach().numpy()
+    return yhat
+
+# prepare the data
+path = 'diabetes.csv'
+train_dl, test_dl = prepare_data(path)
+print(len(train_dl.dataset), len(test_dl.dataset))
+# define the network
+model = MLP(8)
+# train the model
+train_model(train_dl, model)
+# evaluate the model
+acc = evaluate_model(test_dl, model)
+print('Accuracy: %.3f' % acc)
+# make a single prediction (expect class=1)
+row = [0, 96, 100, 23, 94, 20.3, 1.5, 21]
+yhat = predict(row, model)
+print('Predicted: %.3f (class=%d)' % (yhat, yhat.round()))
